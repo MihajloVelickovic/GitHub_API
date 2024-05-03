fbce5479b55c5b2d6207007d61b5b81b7139045d
MihajloVelickovic <mvelickovic03@gmail.com>
2024-05-04 00:53:09 +0200

    Izmene u Cache.cs

--- GitHub_API/Cache.cs
+++ GitHub_API/Cache.cs
@@ -1,20 +1,20 @@
 namespace GitHub_API;
 
 public static class Cache {
-    private static readonly ReaderWriterLockSlim cacheLock = new();
-    private static readonly Dictionary<string, List<GitHubResult>?> cache = new();
+    private static readonly ReaderWriterLockSlim CacheLock = new();
+    private static readonly Dictionary<string, List<GitHubResult>?> CacheDict = new();
 
     public static bool Contains(string key){
-        cacheLock.EnterReadLock();
-        var test =  cache.ContainsKey(key);
-        cacheLock.ExitReadLock();
+        CacheLock.EnterReadLock();
+        var test =  CacheDict.ContainsKey(key);
+        CacheLock.ExitReadLock();
         return test;
     }
     
     public static List<GitHubResult>? ReadFromCache(string key){
-        cacheLock.EnterReadLock();
+        CacheLock.EnterReadLock();
         try{
-            if (cache.TryGetValue(key, out List<GitHubResult>? value))
+            if (CacheDict.TryGetValue(key, out List<GitHubResult>? value))
                 return value;
             else
                 throw new KeyNotFoundException($"Kljuc ({key}) nije pronadjen");
@@ -24,53 +24,21 @@ public static class Cache {
             throw;
         }
         finally{
-            cacheLock.ExitReadLock();
+            CacheLock.ExitReadLock();
         }
     }
 
     public static void WriteToCache(string key, List<GitHubResult>? value){
-        cacheLock.EnterWriteLock();
+        CacheLock.EnterWriteLock();
         try{
-            cache[key] = value;
+            CacheDict[key] = value;
         }
         catch (Exception e){
             Console.Write(e.Message);
             throw;
         }
         finally{
-            cacheLock.ExitWriteLock();
-        }
-    }
-    public static bool WriteToCacheWithTimeout(string key, List<GitHubResult>? value, int timeout){
-        if (cacheLock.TryEnterWriteLock(timeout)){
-            try{
-                cache[key] = value;
-                return true;
-            }
-            catch (Exception e){
-                Console.Write(e.Message);
-                return false;
-            }
-            finally{
-                cacheLock.ExitWriteLock();
-            }
-        }
-        else{
-            Console.WriteLine("Ulaz u WriteLock prevazisao timeout vreme");
-            return false;
-        }
-    }
-    public static void RemoveFromCache(string key){
-        cacheLock.EnterWriteLock();
-        try{
-            cache.Remove(key);
-        }
-        catch (Exception e){
-            Console.WriteLine(e.Message);
-            throw;
-        }
-        finally{
-            cacheLock.ExitWriteLock();
+            CacheLock.ExitWriteLock();
         }
     }
     
