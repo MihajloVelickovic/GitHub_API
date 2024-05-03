4ad4b860c53e3fdef9876ed575b97a914709fbff
MihajloVelickovic <mvelickovic03@gmail.com>
2024-05-04 00:50:07 +0200

    Gotov projekat 1.0

--- GitHub_API/Cache.cs
+++ GitHub_API/Cache.cs
@@ -1,5 +1,77 @@
 namespace GitHub_API;
 
-public static class Cache{
+public static class Cache {
+    private static readonly ReaderWriterLockSlim cacheLock = new();
+    private static readonly Dictionary<string, List<GitHubResult>?> cache = new();
+
+    public static bool Contains(string key){
+        cacheLock.EnterReadLock();
+        var test =  cache.ContainsKey(key);
+        cacheLock.ExitReadLock();
+        return test;
+    }
+    
+    public static List<GitHubResult>? ReadFromCache(string key){
+        cacheLock.EnterReadLock();
+        try{
+            if (cache.TryGetValue(key, out List<GitHubResult>? value))
+                return value;
+            else
+                throw new KeyNotFoundException($"Kljuc ({key}) nije pronadjen");
+        }
+        catch (Exception e){
+            Console.Write(e.Message);
+            throw;
+        }
+        finally{
+            cacheLock.ExitReadLock();
+        }
+    }
+
+    public static void WriteToCache(string key, List<GitHubResult>? value){
+        cacheLock.EnterWriteLock();
+        try{
+            cache[key] = value;
+        }
+        catch (Exception e){
+            Console.Write(e.Message);
+            throw;
+        }
+        finally{
+            cacheLock.ExitWriteLock();
+        }
+    }
+    public static bool WriteToCacheWithTimeout(string key, List<GitHubResult>? value, int timeout){
+        if (cacheLock.TryEnterWriteLock(timeout)){
+            try{
+                cache[key] = value;
+                return true;
+            }
+            catch (Exception e){
+                Console.Write(e.Message);
+                return false;
+            }
+            finally{
+                cacheLock.ExitWriteLock();
+            }
+        }
+        else{
+            Console.WriteLine("Ulaz u WriteLock prevazisao timeout vreme");
+            return false;
+        }
+    }
+    public static void RemoveFromCache(string key){
+        cacheLock.EnterWriteLock();
+        try{
+            cache.Remove(key);
+        }
+        catch (Exception e){
+            Console.WriteLine(e.Message);
+            throw;
+        }
+        finally{
+            cacheLock.ExitWriteLock();
+        }
+    }
     
 }
--- GitHub_API/Program.cs
+++ GitHub_API/Program.cs
@@ -1,47 +1,41 @@
 ﻿using System.Net;
 using System.Net.Http.Headers;
+using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace GitHub_API{
     public class Program{
 
-        public static readonly HttpClient httpClient = new HttpClient();
-
+        public static readonly HttpClient HttpClient = new();
+        
         public static void Main(string[] args){
-
-            /* Uzimanje github tokena iz .env fajla */
             var baseDir = DirExtension.ProjectBase();
             if (baseDir != null){
                 var path = Path.Combine(baseDir, ".env");
                 DotEnv.Inject(path);
             }
-
-            /*
-             * HttpListener na localhostu, zahtevi ce se parsirati
-             * i prevoditi u zahteve GitHub API-u
-             */
+            
+            HttpClient.DefaultRequestHeaders.Add("User-Agent", "GitHub_API");
+            var ghToken = Environment.GetEnvironmentVariable("GH_TOKEN");
+            HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ghToken);
+            
             var listener = new HttpListener();
             listener.Prefixes
-                .Add("http://localhost:1738/");
+                    .Add("http://localhost:1738/");
             listener.Start();
 
             while (true)
                 ThreadPool.QueueUserWorkItem(ServeRequest, listener.GetContext());
-
-
+            
         }
-
         private static void ServeRequest(object? state){
-
             if (state == null)
                 return;
 
             var context = (HttpListenerContext)state;
 
             try{
-
-                /* Izvlacenje vrednosti za owner i repo */
                 var vars = context.Request
                     .Url?
                     .Query
@@ -61,23 +55,26 @@ namespace GitHub_API{
                 if (repo[0] != "repo")
                     throw new Exception("Drugi argument mora biti repo");
 
-                /* Treba pogledati jos */
-                string apiUrl = $"https://api.github.com/repos/{owner[1]}/{repo[1]}/stats/contributors";
+                List<GitHubResult>? contributors;
 
-                /* Podesavanje klijenta da salje korektan zahtev */
-                httpClient.DefaultRequestHeaders.Add("User-Agent", "GitHub_API");
-                var ghToken = Environment.GetEnvironmentVariable("GH_TOKEN");
-                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ghToken);
-                var res = httpClient.GetAsync(apiUrl).Result;
-
-                if (!res.IsSuccessStatusCode)
-                    throw new Exception($"ERROR: {res.StatusCode}");
-
-                var content = res.Content.ReadAsStringAsync().Result;
-                var contributors = JsonConvert.DeserializeObject<List<GitHubResult>>(content);
+                var key = $"{owner[1]}/{repo[1]}";
                 
-                Console.WriteLine($"{owner[1]}/{repo[1]}");
-                int totalCommits = 0;
+                if (Cache.Contains(key))
+                    contributors = Cache.ReadFromCache(key);
+                
+                else{
+                    var apiUrl = $"https://api.github.com/repos/{owner[1]}/{repo[1]}/stats/contributors";
+                    var res = HttpClient.GetAsync(apiUrl).Result;
+                    if (!res.IsSuccessStatusCode)
+                        throw new Exception($"ERROR: {res.StatusCode}");
+
+                    var content = res.Content.ReadAsStringAsync().Result;
+                    contributors = JsonConvert.DeserializeObject<List<GitHubResult>>(content);
+                    Cache.WriteToCache(key, contributors);
+                }
+                
+                Console.WriteLine(key);
+                var totalCommits = 0;
                 foreach (var contributor in contributors!){
                     Console.WriteLine($"{contributor.Author!.Login}: {contributor.Total} commits");
                     totalCommits += contributor.Total;
